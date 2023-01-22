412ed0684d450fd3937c9d2336764e8c788c1808
Sacha Setruk <72971895+Sachou1234567890@users.noreply.github.com>
2023-01-22 22:30:36 +0100

    Revert "Ghassen"

--- app/src/main/java/com/example/rsdev/data/ReceivedMessage.kt
+++
@@ -1,7 +0,0 @@
-package com.example.rsdev.data
-
-
-
-data class ReceivedMessage (val messageText: String, val dateMessage: String, val receiverFirstName: String, val receiverLastName: String) {
-
-}
--- app/src/main/java/com/example/rsdev/data/SentMessage.kt
+++
@@ -1,5 +0,0 @@
-package com.example.rsdev.data
-
-data class SentMessage (val messageText: String, val dateMessage: String, val senderFirstName: String, val senderLastName: String) {
-
-}
